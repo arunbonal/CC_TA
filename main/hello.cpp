edddb94ebfc254629e2bcb66dfb9c7c27eebb652
Arun Bonal <arunbonal516@gmail.com>
2025-03-12 20:30:48 +0530

    made changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,9 +1,10 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
+int main(void)
+{
+  cout << "Hello, World from PES\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
